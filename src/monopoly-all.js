8f3775d9054139dadc55076ed15b242a81a7bf76
wubfile <deletefile.hunter@gmail.com>
2017-09-13 02:58:43 +0200

    The game is fully working, only thing is move the pieces on th table and tests

--- src/monopoly-all.js
+++ src/monopoly-all.js
@@ -69,7 +69,6 @@ square[38] = new Square(38,"LUXURY TAX");
 square[39] = new Square(39,"Boardwalk");
 //class monopoly
 var Monopoly = function(rounds,players){
-  this.bank     = 99999999999;
   this.rounds   = rounds;
   this.players  = players;
   this.player   = [];
@@ -79,17 +78,14 @@ var Monopoly = function(rounds,players){
   var total     = 0;
   var num       = 0;
   this.setPlayers = function(){
+    swapDiv('first_div','second_div');
     for(var i=1; i<=this.players; i++){
-    /*var nameFromInput   = document.getElementById("name").value;
-      var colorFromInput  = document.getElementById("color").value;
-      var moneyFromInput  = document.getElementById("money").value;*/
-      var nameFromInput   = 89;
-      var colorFromInput  = 89;
-      var moneyFromInput  = 89;
-		  this.player[i]  = new Player(nameFromInput, colorFromInput, moneyFromInput);
+      var nameFromInput   = document.getElementById("player_name"+i).value;
+      //var colorFromInput  = document.getElementById("player_color"+i).value;
+      this.player[i]  = new Player(nameFromInput, "red", 1500);
       //all players are set at GO by default "this.pos = 0;"
     }
-    console.log("[*]Giocatori generati correttamente!");
+    document.getElementById("second_div").innerHTML += "[*]Giocatori generati correttamente!<br>";
   }
   this.rollDice = function(){
 
@@ -109,10 +105,10 @@ var Monopoly = function(rounds,players){
         this.player[i].setPosition(this.player[i].getPlayerPos()+total-39);
         //give 200€ for pasing go or landing on go
         this.moneyToPlayer(200,i);
-        console.log(i+")"+this.player[i].pos);
+          document.getElementById("second_div").innerHTML += "("+this.player[i].name+")"+this.player[i].pos+"<br>";
       }else{
         this.player[i].setPosition(this.player[i].getPlayerPos()+total);
-      console.log(i+")"+this.player[i].pos);
+          document.getElementById("second_div").innerHTML += "("+this.player[i].name+")"+this.player[i].pos+"<br>";
     }
       if(this.player[i].getPlayerPos() == 30){
         //the player is in jail
@@ -139,27 +135,73 @@ var Monopoly = function(rounds,players){
     this.player[id].setPlayerMoney(this.player[id].money-amount);
   }
 };
-playRound = function(){
-  var numPlayers = 5;
+playGame = function(number){
+  var numPlayers = number;
     if(numPlayers>=2 && numPlayers<=8){
       monopoly = new Monopoly(20,numPlayers);
-      console.log("[*]Genero "+monopoly.players+" giocatori.");
-      console.log("[*]Setto i giocatori.");
+      document.getElementById("second_div").innerHTML += "[*]Genero "+monopoly.players+" giocatori.<br>";
+      document.getElementById("second_div").innerHTML += "[*]Setto i giocatori.<br>";
       monopoly.setPlayers();
+      var k = 1;
+      playRound(k);
 
-      for(var k=1; k<=monopoly.rounds;k++){
-        console.log("["+k+" Round]");
-
-          for(var i=1; i<=monopoly.players; i++){
-            monopoly.rollDice();
-            monopoly.movePlayer(i);
-            monopoly.resetDice();
-          }
-      }
     }else{
-      console.log("Set a number between 2 and 8.");
+      alert("Set a number between 2 and 8.");
+      document.getElementById("pl").innerHTML = "";
     }
 }
+
+playRound = function(asd){
+  var k = asd;
+
+    if (k<=20){
+      document.getElementById("second_div").innerHTML = "["+k+"] Round<br>";
+        for(var i=1; i<=monopoly.players; i++){
+          monopoly.rollDice();
+          monopoly.movePlayer(i);
+          monopoly.resetDice();
+        }
+      k++;
+      document.getElementById("second_div").innerHTML += "<input type='button' value='set' onclick='playRound("+k+")' />";
+
+  }else{
+    alert("Finiti i 20 turni");
+    var max=0;
+    var id=0;
+    for(var i=1; i<=monopoly.players; i++){
+      if (monopoly.player[i].money>=max){
+        max = monopoly.player[i].money;
+        id = i;
+      }
+    }
+    alert("Il vincitore è "+monopoly.player[id].name+" che ha €"+monopoly.player[id].money);
+  }
+}
+
+
+
+swapDiv = function(first_div,second_div){
+
+   d1 = document.getElementById(first_div);
+   d2 = document.getElementById(second_div);
+    if( d2.style.display == "none" ){
+      d1.style.display = "none";
+      d2.style.display = "block";
+    }else{
+      d1.style.display = "block";
+      d2.style.display = "none";
+    }
+}
+    function generateTextBoxes() {
+      var a = parseInt(document.getElementById("n_players").value);
+        for (i = 1; i <= a; i++) {
+          var texte = "Player "+i+") <input type='text' id='player_name"+i+"'/><br>";
+          document.getElementById("pl").innerHTML += texte;
+        }
+        document.getElementById("pl").innerHTML += "<input type='button' value='set' onclick='playGame("+a+")'>";
+    }
+
+
 
 /*
 posizionare pedina
